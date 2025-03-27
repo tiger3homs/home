d60d586ae9909bab3e50bdd7c59dc73aa0196c3b
Obida Sabbagh <tiger3homs@gmail.com>
2025-03-27 21:58:58 +0000

    sync

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/home/', // Set the base path to match the GitHub Pages deployment
+  base: process.env.NODE_ENV === 'production' ? './' : '/', // Set the base path to match the GitHub Pages deployment
   plugins: [react()],
 });
