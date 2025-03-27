0541b545ded25942606c23446964ad5bc5b30a9f
Obida Sabbagh <tiger3homs@gmail.com>
2025-03-27 21:40:37 +0000

    Uppdate

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // Set the base path to root
+  base: './',
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
 });
