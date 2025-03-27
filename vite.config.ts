b4d009440144b31910bb5b30512c32502c7f2b7a
Obida Sabbagh <tiger3homs@gmail.com>
2025-03-27 21:48:56 +0000

    Sync

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/home/', // Set the base path to match the GitHub Pages deployment
   plugins: [react()],
-  optimizeDeps: {
-    // Remove 'lucide-react' from the exclude array
-  },
 });
