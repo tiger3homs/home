4b182317e8814f7e8c60c5d2118a6a74c7c014e6
Obida Sabbagh <tiger3homs@gmail.com>
2025-03-27 21:38:27 +0000

    Uppdate

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/home/', // Match the "homepage" field in package.json
+  base: '/', // Set the base path to root
   plugins: [react()],
 });
