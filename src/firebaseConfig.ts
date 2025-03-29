6b534cfe51976b389dd97919fda9f8551331b0fe
Obida Sabbagh <tiger3homs@gmail.com>
2025-03-29 23:58:58 +0000

    uppdate-firebase-info

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -2,13 +2,13 @@ import { initializeApp, FirebaseApp } from "firebase/app";
 import { Auth, getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDr3k3DjPk8fNlgapY3Gge4ngXXuRifDjA",
-  authDomain: "tiger3homs-f3a5f.firebaseapp.com",
-  projectId: "tiger3homs-f3a5f",
-  storageBucket: "tiger3homs-f3a5f.firebasestorage.app",
-  messagingSenderId: "565046427467",
-  appId: "1:565046427467:web:7188f79739eaa50473f2ce",
-  measurementId: "G-Z4TSDCDD4R"
+  apiKey: "AIzaSyAsGrtbRSjz77iShv4tB0vQ33A23ie9JM8",
+  authDomain: "tiger3homs-f3a5f-d60da.firebaseapp.com",
+  projectId: "tiger3homs-f3a5f-d60da",
+  storageBucket: "tiger3homs-f3a5f-d60da.firebasestorage.app",
+  messagingSenderId: "509990310606",
+  appId: "1:509990310606:web:e36d91ea748c4cd64d369f",
+  measurementId: "G-DDZTZ6MR6S"
 };
 
 let app: FirebaseApp | null = null;
